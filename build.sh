775f8b75e92ae51a69d6c6fc297751d93869a83d
Michael Robinson <mike@pagesofinterest.net>
2013-02-26 21:08:57 +1300

    Added build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git submodule init
+git submodule update
+
+# Make sure node-gyp is available before starting
+hash node-gyp &> /dev/null
+if [ $? -eq 1 ]; then
+    echo "node-gyp is required, and must be available on the path"
+    exit 1
+fi
+
+mkdir -p deps/libgit2/build && \
+    cd deps/libgit2/build && \
+    cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=false -D BUILD_CLAR=false .. && \
+    cmake --build . && \
+    cd ../../.. && \
+    node-gyp rebuild
